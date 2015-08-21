b3b5fb062b687b5ab5307c1eaee97bcbddc8c33f
Rainer M. Krug <R.M.Krug@gmail.com>
2015-08-21 15:31:46 +0200

    Preparation for multiple os
    * use os: instead of -languange
    * use $TRAVIS_OS_NAME instead of #osx# in calling the scripts. This
      enables easy usage of .travis.linux.*.sh scripts for linux testing.

--- .travis.osx.install.sh
+++ .travis.osx.install.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env sh
 
 ## install dependencies
-brew install $INSTALLOPTIONS --only-dependencies $RECIPE
+brew install --only-dependencies \
+             --HEAD \
+             --with-nc_spm_08_grass7 \
+             --with-ffmpeg \
+             --with-mysql \
+             --with-netcdf \
+             --with-postgresql \
+             --with-openblas \
+             --with-liblas \
+             grass-71
 
 ## download and install test dataset
 # wget http://grass.osgeo.org/sampledata/north_carolina/nc_basic_spm_grass7.tar.gz
--- .travis.osx.script.sh
+++ .travis.osx.script.sh
@@ -3,12 +3,21 @@
 brew audit -v $RECIPE
 
 ## install GRASS 7.1 
-brew install $INSTALLOPTIONS $RECIPE
-brew test -v $RECIPE
-brew info $RECIPE
+brew install --HEAD \
+             --with-nc_spm_08_grass7 \
+             --with-ffmpeg \
+             --with-mysql \
+             --with-netcdf \
+             --with-postgresql \
+             --with-openblas \
+             --with-liblas \
+             grass-71
+
+brew test -v grass-71
+brew info grass-71
 
 ## run tests
 ## grass71 ./nc_basic_spm_grass7/PERMANENT --exec python -m grass.gunittest.main --location './nc_basic_spm_grass7' --location-type nc
 
 ## uninstall grass-71
-brew uninstall $RECIPE
+brew uninstall grass-71
