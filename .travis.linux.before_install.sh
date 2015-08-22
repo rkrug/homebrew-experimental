9ed7734f80d36204ce04ee5ba59ac122c9b0d4d9
Rainer M. Krug <R.M.Krug@gmail.com>
2015-08-22 15:00:37 +0200

    Add scripts for linux
    * added linux as os
    * used code from
      https://github.com/GRASS-GIS/grass-ci/blob/master/.travis.yml and
      transfered into .travis.linux.*.sh scripts

--- .travis.linux.before_install.sh
+++ .travis.linux.before_install.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env sh
+# build dependencies
+sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable -y
+sudo apt-get update -qq
+
+# test dataset
+wget http://grass.osgeo.org/sampledata/north_carolina/nc_basic_spm_grass7.tar.gz
+tar xzf ./nc_basic_spm_grass7.tar.gz
