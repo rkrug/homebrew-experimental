fcde2a1c6af356d9be0de91d048a2ec5f0785421
Rainer M. Krug <R.M.Krug@gmail.com>
2015-08-27 14:30:56 +0200

    Manual reverting to only grass-71 test

--- .travis.linux.before_install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# build dependencies
-sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable -y
-sudo apt-get update -qq
-
--- .travis.osx.before_install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-## add taps for homebrew installs
-brew tap rkrug/experimental
-brew tap rkrug/head-only
-brew tap rkrug/dev-only
-brew tap homebrew/science
-brew update
